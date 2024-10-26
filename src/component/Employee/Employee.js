823b7c49782e90e852f09b184f3205c174616175
Prince Yadav <prince.yadav@fastcurveservices.com>
2024-10-26 21:51:13 +0530

    forms-files-changes

--- src/component/Employee/Employee.js
+++ src/component/Employee/Employee.js
@@ -7,7 +7,20 @@ const Employee = () => {
   return (
     <>
     <Header/>
-    <div>Employee</div>
+    <div class="employee-wrapper ">
+      <div className="container">
+        <h1>Upload Your File</h1>
+        <form action="#" method="post" enctype="multipart/form-data">
+            <div class="upload-area">
+                <label for="file" class="upload-label">
+                    Drag & Drop your files here or <span class="browse">Browse</span>
+                    <input type="file" name="file" id="file" required/>
+                </label>
+            </div>
+            <button type="submit">Upload</button>
+        </form>
+        </div>
+    </div>
     <Footer/>
     </>
   )
--- src/component/Employer/Employer.js
+++ src/component/Employer/Employer.js
@@ -7,7 +7,30 @@ const Employer = () => {
   return (
     <>
                <Header/>
-               <div>Employer</div>
+               <div className='employer-wrapper'>
+               <div class="container">
+        <h1>User Information</h1>
+        <form action="#" method="post">
+            <div class="form-group">
+                <label for="firstName">First Name</label>
+                <input type="text" id="firstName" name="firstName" required/>
+            </div>
+            <div class="form-group">
+                <label for="lastName">Last Name</label>
+                <input type="text" id="lastName" name="lastName" required/>
+            </div>
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input type="email" id="email" name="email" required/>
+            </div>
+            <div class="form-group">
+                <label for="graduationMarks">Graduation Marks</label>
+                <input type="number" id="graduationMarks" name="graduationMarks" required/>
+            </div>
+            <button type="submit">Submit</button>
+        </form>
+    </div>
+               </div>
                 <Footer/>
                </>
   )
